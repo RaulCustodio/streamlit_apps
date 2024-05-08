c447240a477ddfd195e17122bd9548009b4b0b3e
RaulCustodio <raulcustodiojunior@gmail.com>
2024-05-08 20:26:57 -0300

    tech4.py

--- tech4.py
+++ tech4.py
@@ -1,6 +1,9 @@
 import  streamlit as st
 import requests
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from statsmodels.tsa.seasonal import seasonal_decompose
+!pip install statsforecast
 
-
-st.title('DASHBOARD DE PREVISÃO DE PREÇO DO PETRÓLEO3')
+st.title('DASHBOARD DE PREVISÃO DE PREÇO DO PETRÓLEO')
